dac6db37d5c98eea754fe81e80536dfef15eed70
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2026-02-28 01:37:14 +0100

    feat: removed the old resume, and commented out the resume button

--- src/components/atoms/social.tsx
+++ src/components/atoms/social.tsx
@@ -1,18 +1,20 @@
 import { FaLinkedin, FaGithubSquare } from "react-icons/fa"
-import { TbFileCv } from "react-icons/tb"
+//import { TbFileCv } from "react-icons/tb"
 
 const Social: React.FC = () => {
   return (
     <div className="mr-10 flex justify-center space-x-4 lg:justify-start">
-      <a href="https://linkedin.com/in/oliver-n-hansen-a268922a7" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 text-3xl hover:scale-110">
+        <a href="https://linkedin.com/in/oliver-n-hansen-a268922a7" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 text-3xl hover:scale-110">
         <FaLinkedin />
       </a>
-      <a href="https://github.com/oliv6362" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 text-3xl hover:scale-110">
+        <a href="https://github.com/oliv6362" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 text-3xl hover:scale-110">
         <FaGithubSquare />
       </a>
-      <a href="resume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 text-3xl hover:scale-110">
+        {/*
+        <a href="resume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 text-3xl hover:scale-110">
         <TbFileCv />
       </a>
+        */}
     </div>
   )
 }
